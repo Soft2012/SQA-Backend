dc8fc03f36b50f94b6728beb6a48fb199c1753b5
Soft2012 <TeamClick2012@outlook.com>
2024-09-03 08:12:36 -0400

    modify for deploy

--- index.js
+++ index.js
@@ -17,41 +17,34 @@ app.post("/test", (req,res)=>{
 
   // Spawn a Python process to run the script
   const pythonProcess = spawn('python', ['script.py', `${fileContent}`]);
-
-  console.log("Generating...............")
-
-  res.send({
-    text: fileContent,
+  let hasResponseSent = false; 
+  // Handle the script's output (stdout)
+  pythonProcess.stdout.on('data', (data) => {
+    if (!hasResponseSent) {
+        console.log(`Finished................`);
+        hasResponseSent = true;
+        res.send({
+          text: data.toString(),
+        });
+      }
   });
 
-  // let hasResponseSent = false; 
-  // // Handle the script's output (stdout)
-  // pythonProcess.stdout.on('data', (data) => {
-  //   if (!hasResponseSent) {
-  //       console.log(`Finished................`);
-  //       hasResponseSent = true;
-  //       res.send({
-  //         text: data.toString(),
-  //       });
-  //     }
-  // });
+  // Handle any errors (stderr)
+  pythonProcess.stderr.on('data', (data) => {
+    console.error(`Error: ${data.toString()}`);
+    if (!hasResponseSent) {
+      hasResponseSent = true;
+      res.status(500).send("An error occurred while processing the script.");
+    }
+  });
 
-  // // Handle any errors (stderr)
-  // pythonProcess.stderr.on('data', (data) => {
-  //   console.error(`Error: ${data.toString()}`);
-  //   if (!hasResponseSent) {
-  //     hasResponseSent = true;
-  //     res.status(500).send("An error occurred while processing the script.");
-  //   }
-  // });
-
-  // // Handle the close event when the script finishes
-  // pythonProcess.on('close', (code) => {
-  //   console.log(`Python script exited with code ${code}`);
-  //   if (!hasResponseSent) {
-  //     res.end(); // Only call res.end() if no other response has been sent.
-  //   }
-  // });
+  // Handle the close event when the script finishes
+  pythonProcess.on('close', (code) => {
+    console.log(`Python script exited with code ${code}`);
+    if (!hasResponseSent) {
+      res.end(); // Only call res.end() if no other response has been sent.
+    }
+  });
 })
 
 app.listen(port, () => {
