a164c5350c05b6128322f1b3d0383e99878a0cf9
Soft2012 <TeamClick2012@outlook.com>
2024-09-12 04:14:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const port = 5000;
 
 // Middleware
-app.use(cors({ origin: 'https://automatic-testcase-generation.vercel.app' }));
+app.use(cors());
 app.use(bodyParser.json());
 
 app.post("/test", async (req,res)=>{
@@ -19,52 +19,16 @@ app.post("/test", async (req,res)=>{
   try {
     const test_cases = await openaiService.chatgptAPIRequest(prompt)
     console.log(test_cases)
-    const testcases_obj = await JSON.parse(test_cases);
-
-    const test_case_list = testcases_obj["test_cases"]
-
-    let out_put_string = ''
-    let id = 1
-    for (const test_case of test_case_list)
-    {
-      let test_step_string = ""
-      const test_step = test_case["Test Steps"]
-      let step_id = 1
-      for (const step of test_step)
-      {
-        test_step_string += `${step_id}. ` + step + ".\n"
-        step_id++
-      }
-
-      const data_object = test_case["Data to use"]
-      let data_to_use = '';
-      let data_to_use_id = 1;
-      Object.entries(data_object).forEach(([key, value]) => {
-        // console.log(`Key: ${key}, Value: ${value}`);
-        data_to_use += `${data_to_use_id}. ${key} : ${value}\n`
-        data_to_use_id++
-      });
-      // console.log("data to use:\n", data_to_use)
-      
-      let testcase_string = (
-        `Test Case ${id}\n` +
-        `Title: ${test_case["Title"]}\n` +
-        `Description: ${test_case["Description"]}\n` +
-        `Precondition: ${test_case["Preconditions"]}\n` +
-        `Test Steps:\n${test_step_string}` +
-        `Expected Outcome: ${test_case["Expected Outcome"]}\n` +
-        `Data to use:\n${data_to_use}` +
-        `Priority: ${test_case["Priority"]}\n\n`)
-      
-      out_put_string += testcase_string
-      id++;
+    try {
+      const testcases_obj = await JSON.parse(test_cases);
+      res.send({testcases:testcases_obj})
+    } catch (error) {
+      console.error('Json Parse Error:', error.response ? error.response.data : error.message);
     }
-
-    // Add CORS headers
-    res.setHeader('Access-Control-Allow-Origin', 'https://automatic-testcase-generation.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.send({text:out_put_string})
+    // // Add CORS headers
+    // res.setHeader('Access-Control-Allow-Origin', 'https://automatic-testcase-generation.vercel.app');
+    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    // res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   } 
   catch (error) {
       console.error('Error calling ChatGPT API:', error.response ? error.response.data : error.message);
